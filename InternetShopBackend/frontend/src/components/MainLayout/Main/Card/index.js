26741b07097c0b9673114aa691d1ebc140426498
root <root@vm29110.onevdc.net>
2023-07-13 21:28:26 +0300

    Load data

--- InternetShopBackend/frontend/src/components/MainLayout/Main/Card/index.js
+++ InternetShopBackend/frontend/src/components/MainLayout/Main/Card/index.js
@@ -9,17 +9,16 @@ import { useEffect, useState } from "react";
 import classNames from "classnames";
 
 
-export const CardWA = ({image, brand, price, title, id}) => {
+export const CardWA = ({ image, brand, price, title, id }) => {
 
     const navigate = useNavigate();
     const [productId, setProductId] = useCookie('pr_id', 0);
 
-    return (<div id="product1" data-id={id} onClick={() => 
-    {
-        window.scrollTo(0,0);
+    return (<div id="product1" data-id={id} onClick={() => {
+        window.scrollTo(0, 0);
         setProductId(id);
         navigate("/product")
-        
+
     }}>
         <div className="pro">
             <img src={image} />
@@ -33,17 +32,22 @@ export const CardWA = ({image, brand, price, title, id}) => {
 }
 
 
-const Card = ({image, brand, price, title, id}) => {
+const Card = ({ image, brand, price, title, id }) => {
+
+    const [settings, setSettings] = useState({
+        open: false,
+        btnOpen: false,
+        isFlag: false
+    });
     const navigate = useNavigate();
     const [productId, setProductId] = useCookie('pr_id', 0);
     const [sizes, setArrSizes] = useState([]);
-    const [isFlag, setFlag] = useState(false);
     const [isBuy, setBuy] = useState(false);
-
-    const dispatch = useDispatch();
     const carts = useSelector(cart => cart.cart);
 
-    const addToCartProduct = async(size) => {
+    const dispatch = useDispatch();
+
+    const addToCartProduct = async (size) => {
         let res = await axiosService.getProductById(id);
         dispatch(addToCart({
             id: res.id,
@@ -51,11 +55,15 @@ const Card = ({image, brand, price, title, id}) => {
             price: res.price,
             description: res.description,
             image: image,
-            size: parseInt(sizes[size-1].size)
+            size: parseInt(sizes[size - 1].size)
         }));
 
-        setFlag(!isFlag);
-        if(isBuy) {
+        setSettings({
+            ...settings,
+            isFlag: !settings.isFlag,
+            btnOpen: false,
+        });
+        if (isBuy) {
             navigate("/order")
         }
     }
@@ -64,7 +72,10 @@ const Card = ({image, brand, price, title, id}) => {
         e.preventDefault();
         e.stopPropagation();
         fillSizes();
-        setFlag(!isFlag);
+        setSettings({
+            ...settings,
+            isFlag: !settings.isFlag
+        });
         setBuy(true);
     }
 
@@ -72,7 +83,10 @@ const Card = ({image, brand, price, title, id}) => {
         e.preventDefault();
         e.stopPropagation();
         fillSizes();
-        setFlag(!isFlag);
+        setSettings({
+            ...settings,
+            isFlag: !settings.isFlag
+        });
         setBuy(false);
     }
 
@@ -80,10 +94,10 @@ const Card = ({image, brand, price, title, id}) => {
         let item = e.target.closest(".section-p1");
         let id = item.dataset.id;
         let par = e.target.closest(".product-card");
-        if(par.offsetHeight == 450) {
+        if (par.offsetHeight == 450) {
 
             setProductId(id);
-            window.scrollTo(0,0);
+            window.scrollTo(0, 0);
             navigate("/product")
         }
     }
@@ -97,37 +111,45 @@ const Card = ({image, brand, price, title, id}) => {
     const onSelectItem = (e) => {
         let value = e.target.value;
         addToCartProduct(value);
-        setBtnOpen(false);
     }
 
-    const onSelectClick =(e) => {
+    const onSelectClick = (e) => {
         e.preventDefault();
         e.stopPropagation();
     }
 
     const onMouseHandler = (e) => {
-        if(!btnOpen) {
-
-            if(isFlag) {
-                setFlag(false);
-            }
-            setOpen(false);
+        // if(!settings.btnOpen) {
+        if (settings.isFlag) {
+            setSettings({
+                ...settings,
+                isFlag: !settings.isFlag,
+                open: false,
+            });
+            return;
         }
+        setSettings({
+            ...settings,
+            open: false,
+        });
+        // }
     }
-
-    const [open, setOpen] = useState(false);
-    const [btnOpen, setBtnOpen] = useState(false);
 
     return (<>
 
-        <section id="product1" data-id={id}  onClick={() => 
-    {
-       
-        
-    }} className="section-p1">
-            <div className={classNames("product-card", {"product-card-hover" : open})}
-            onMouseEnter={() => {setOpen(true)}}
-            onMouseLeave={onMouseHandler} onClick={onLinkToProduct}>
+        <section id="product1" data-id={id} onClick={() => {
+
+
+        }} className="section-p1">
+            <div className={classNames("product-card", { "product-card-hover": settings.open })}
+                onMouseEnter={() => {
+                    setSettings({
+                        ...settings,
+                        open: true
+                    })
+                }}
+                onMouseLeave={onMouseHandler}
+                onClick={onLinkToProduct}>
                 <div className="image-container">
                     <img src={image} alt="Product Image" />
                 </div>
@@ -140,34 +162,40 @@ const Card = ({image, brand, price, title, id}) => {
                     </div>
                 </div>
                 <div>
-                    {!isFlag && (carts.filter(x => x.id == id).length == 0 ? <>
-                        <button className="button-buy" 
+
+
+                    <div className={classNames({ "d-none": settings.isFlag}, {"d-none": carts.filter(x => x.id == id).length != 0})}>
+                        <button className="button-buy"
                             onClick={onBuyButtonClick}>ПРИДБАТИ В 1 КЛІК</button>
                         <button className="button-cart"
-                            onClick={onCartButtonClick}>ДОДАТИ В КОШИК</button></>
-                        :
-                        <>
-                            <Alert message="Товар додано у кошик." type="success" />
-                        </>)}
+                            onClick={onCartButtonClick}>ДОДАТИ В КОШИК</button>
+                    </div>
 
-                    { isFlag &&
-                        <select
-                            onMouseDown={() => {
-                                setBtnOpen(true);
-                            }}
-                            style={{
-                                marginTop: '10px',
-                                fontSize: '1.5em'
-                            }}
-                            className="select" onClick={onSelectClick} onChange={onSelectItem}>
-                            <option value={0}>Виберіть розмір</option>
-                            {sizes.map((element, index) => {
-                                return (
-                                    <option value={index + 1} key={"sizeCard" + (index + 1)}>{element.size}</option>
-                                )
-                            })}
-                        </select> 
-                    }
+                    <div className={classNames({ "d-none": settings.isFlag}, { "d-none" : carts.filter(x => x.id == id).length == 0  })}>
+                        <Alert message="Товар додано у кошик." type="success" />
+                    </div>
+
+                    <select
+
+                        onMouseDown={() => {
+                            setSettings({
+                                ...settings,
+                                btnOpen: true
+                            });
+                        }}
+                        style={{
+                            marginTop: '10px',
+                            fontSize: '1.5em'
+                        }}
+                        className={classNames("select", { "d-none": !settings.isFlag })} onClick={onSelectClick} onChange={onSelectItem}>
+                        <option value={0}>Виберіть розмір</option>
+                        {sizes.map((element, index) => {
+                            return (
+                                <option value={index + 1} key={"sizeCard" + (index + 1)}>{element.size}</option>
+                            )
+                        })}
+                    </select>
+
                 </div>
             </div>
         </section>
